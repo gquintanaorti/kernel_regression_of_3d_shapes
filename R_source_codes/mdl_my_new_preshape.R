31520aad308ee7f6815df17681e4be0ae70e253e
gquintanaorti <gquintan@icc.uji.es>
2019-06-04 09:18:58 +0200

    Add files via upload

--- R_source_codes/mdl_my_new_preshape.R
+++ R_source_codes/mdl_my_new_preshape.R
@@ -42,14 +42,16 @@
 
 
 # =============================================================================
-my_new_preshape = function( x ) {
-
-    #### # Set timer.
-    #### t1 = proc.time()[ "elapsed" ]
-
+my_preshape = function( x ) {
+#
+# Written by G. Quintana-Ortí and Amelia Simó, University Jaume I, Spain.
+# It is an optimization for performance of Dryden's original code.
+# This code is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY EXPRESSED OR IMPLIED.
+#
     if (is.complex(x)) {
         k <- nrow(as.matrix(x))
-        h <- my_new_defh(k - 1)
+        h <- my_defh(k - 1)
         zstar <- x
         ztem <- h %*% zstar
         size <- sqrt(diag(Re(st(ztem) %*% ztem)))
@@ -61,63 +63,41 @@ my_new_preshape = function( x ) {
     else {
         if (length(dim(x)) == 3) {
             k <- dim(x)[1]
-            h <- my_new_defh(k - 1)
+            h <- my_defh(k - 1)
             n <- dim(x)[3]
             m <- dim(x)[2]
             z <- array(0, c(k - 1, m, n))
             for (i in 1:n) {
                 z[, , i] <- h %*% x[, , i]
-                size <- my_new_centroid.size(x[, , i])
+                size <- my_centroid.size(x[, , i])
                 z[, , i] <- z[, , i]/size
             }
         }
         else {
             k <- nrow(as.matrix(x))
 
-            #### # Set timer.
-            #### t01 = proc.time()[ "elapsed" ]
-            #### 
-            #### h <- my_new_defh(k - 1)
+            #### h <- my_defh(k - 1)
             #### ztem <- h %*% x
-            #### 
-            #### # Print elapsed time.
-            #### t02 = proc.time()[ "elapsed" ]
-            #### cat( "  1. Elapsed time in hx: ", t02-t01, "\n" )
-            #### 
-            #### # Set timer.
-            #### t01 = proc.time()[ "elapsed" ]
 
-            # Use of my_new_compute_hx_implicitely to accelerate the code.
-            ztem <- my_new_compute_hx_implicitely( x )
+            # Use of multiply_by_helmert_implicitely to accelerate the code.
+            ztem <- multiply_by_helmert_implicitely( x )
 
-            #### # Print elapsed time.
-            #### t02 = proc.time()[ "elapsed" ]
-            #### cat( "  2. Elapsed time in hx: ", t02-t01, "\n" )
-            #### 
-            #### cat( "abs norm de diff: ", 
-            ####      norm( ztem - ztem2, type = "F" ), "\n" )
-            #### cat( "rel norm de diff: ", 
-            ####      norm( ztem - ztem2, type = "F" ) / 
-            ####      norm( ztem, type = "F" ), "\n" )
-            #### cat( "  dim de h:    ", dim( h ), "\n" )
-            #### cat( "  dim de x:    ", dim( x ), "\n" )
-            #### cat( "  dim de ztem: ", dim( ztem ), "\n" )
-
-            size <- my_new_centroid.size(x)
+            size <- my_centroid.size(x)
             z <- ztem/size
         }
     }
-    #### # Print elapsed time.
-    #### t2 = proc.time()[ "elapsed" ]
-    #### cat( "Elapsed time in my_new_preshape (seconds): ", t2 - t1, "\n" )
 
-    return( z )
+    z
 }
 
-
-
 # =============================================================================
-my_new_centroid.size = function( x ) {
+my_centroid.size = function( x ) {
+#
+# Written by G. Quintana-Ortí and Amelia Simó, University Jaume I, Spain.
+# It is an optimization for performance of Dryden's original code.
+# This code is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY EXPRESSED OR IMPLIED.
+#
     if ((is.vector(x) == FALSE) && is.complex(x)) {
         k <- nrow(x)
         n <- ncol(x)
@@ -131,7 +111,7 @@ my_new_centroid.size = function( x ) {
             n <- dim(x)[3]
             sz <- rep(0, times = n)
             k <- dim(x)[1]
-            h <- my_new_defh(k - 1)
+            h <- my_defh(k - 1)
             for (i in 1:n) {
                 xh <- h %*% x[, , i]
                 sz[i] <- sqrt(sum(diag(t(xh) %*% xh)))
@@ -144,10 +124,11 @@ my_new_centroid.size = function( x ) {
             }
             k <- nrow(x)
 
-            #### h <- my_new_defh(k - 1)
+            #### h <- my_defh(k - 1)
             #### xh <- h %*% x
-            # Use of my_new_compute_hx_implicitely to accelerate the code.
-            xh <- my_new_compute_hx_implicitely( x )
+
+            # Use of multiply_by_helmert_implicitely to accelerate the code.
+            xh <- multiply_by_helmert_implicitely( x )
 
             size <- sqrt(sum(diag(t(xh) %*% xh)))
             size
@@ -156,11 +137,13 @@ my_new_centroid.size = function( x ) {
 }
 
 # =============================================================================
-my_new_defh = function( nrow ) {
-
-    #### # Set timer.
-    #### t1 = proc.time()[ "elapsed" ]
-
+my_defh = function( nrow ) {
+#
+# Written by G. Quintana-Ortí and Amelia Simó, University Jaume I, Spain.
+# It is an optimization for performance of Dryden's original code.
+# This code is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY EXPRESSED OR IMPLIED.
+#
     k <- nrow
     h <- matrix( 0, k, k + 1 )
     if( nrow > 0 ) {
@@ -171,23 +154,20 @@ my_new_defh = function( nrow ) {
       }
     }
 
-    #### # Print elapsed time.
-    #### t2 = proc.time()[ "elapsed" ]
-    #### cat( "Elapsed time in my_new_defh (seconds): ", t2 - t1, "\n" )
-
-    return( h )
+    h
 }
 
 # =============================================================================
-my_new_compute_hx_implicitely = function( x ) {
-
-    #### # Set timer.
-    #### t1 = proc.time()[ "elapsed" ]
-
+multiply_by_helmert_implicitely = function( x ) {
+#
+# This code multiplies the "x" argument by the Helmert matrix of the 
+# corresponding size.
+# Written by G. Quintana-Ortí and Amelia Simó, University Jaume I, Spain.
+# This code is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY EXPRESSED OR IMPLIED.
+#
     m = dim( x )[ 1 ]-1
     n = dim( x )[ 2 ]
-
-    #### print( dim( x ) )
 
     result <- matrix( 0, m, n )
     vsum   <- rep( 0, n )
@@ -196,21 +176,12 @@ my_new_compute_hx_implicitely = function( x ) {
         val  = -1 / sqrt( i * ( i + 1 ) )
         hi   = val
         hip1 = - i * val
-        #### cat( "i: ", i, "  hi:", hi, "  hip1: ", hip1, "\n" )
 
-        #### for( j in seq( 1, n ) ) {
-        ####   vsum[ j ]      = vsum[ j ] + x[ i, j ]
-        ####   #### cat( "  j: ", j, " vsum[ j ]: ", vsum[ j ], "\n" )
-        ####   result[ i, j ] = vsum[ j ] *  hi + x[ i+1, j ] * hip1
-        #### }
         vsum = vsum + x[ i, ]
-        result[ i, ] = vsum * hi + x[ i + 1, ] * hip1
+        result[ i, ] = vsum *  hi + x[ i+1, ] * hip1
+
       }
     }
-
-    #### # Print elapsed time.
-    #### t2 = proc.time()[ "elapsed" ]
-    #### cat( "Elapsed time in my_new_defh (seconds): ", t2 - t1, "\n" )
 
     return( result )
 }
